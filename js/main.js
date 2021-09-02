68cf5d5fa2b32e9cefbc51d37735a42c458bba72
minervamadrigal <minervamadrigal@protonmail.com>
2021-09-02 16:19:10 -0500

    testing

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@ let main = document.getElementById("container");
 let dataRaw = [];
 let dataArray = {};
 let urlTrivia = "";
-let jsonCache = "";
 let score = 0;
 let indice = 0;
 let lengthArray = 0;
+let answers = [];
+
 
 formMain.addEventListener("submit", function(e){
     e.preventDefault();
     dataRaw = [];
+    answers = [];
     let data = new FormData(formMain);
     for(const [name, value] of data){        
         dataRaw.push(value);
@@ -20,6 +22,10 @@ formMain.addEventListener("submit", function(e){
     }
     urlTrivia = makeUrl(dataRaw);
     console.log(urlTrivia);
+    for(let i = 0; i <dataRaw[0]; i++){
+        answers.push(null); 
+    }    
+    console.log(answers)
     handlerTrivia(urlTrivia);
 });
 
@@ -28,8 +34,7 @@ async function handlerTrivia(urlTrivia) {
     console.log(response);
     dataArray = await response.json();
     console.log(dataArray.results);    
-    renderList(); 
-    
+    checkArray(dataArray);     
 };
 
 function makeUrl(data){
@@ -48,20 +53,19 @@ function makeUrl(data){
     return partialUrl;
 }
 
-const renderList = () => {
+const renderList = (indexBtn) => {
     removeContainers("container");    
-    renderTrivia(dataArray, indice);
+    renderTrivia(dataArray, indice, indexBtn);
     addClickTrivia();    
     previousQuestion();
     nextQuestion();
 };
 
-function renderTrivia(dataArray, index){
+function renderTrivia(dataArray, index, indexBtn){
     let question = dataArray.results[index];
     const header = document.createElement("h2");
     header.innerText = "Trivia";
     header.classList.add("headerTrivia")
-    /*main.classList.add("hid");*/
     
     const questionContainer = document.createElement("div");
     questionContainer.classList.add("questionDiv");
@@ -80,22 +84,25 @@ function renderTrivia(dataArray, index){
 
     const buttonContainer = document.createElement("div");
     buttonContainer.classList.add("actions");
+    buttonContainer.setAttribute("id", `${indexBtn}`);
 
     let questionButton1 = document.createElement("button");
-    questionButton1.innerText = question.correct_answer;
+    questionButton1.innerText = question.correct_answer;  
+
+    const questionButton2 = document.createElement("button");
+    questionButton2.innerText = question.incorrect_answers[0];    
+
+    const questionButton3 = document.createElement("button");
+    questionButton3.innerText = question.incorrect_answers[1];
+
+    const questionButton4 = document.createElement("button");
+    questionButton4.innerText = question.incorrect_answers[2];    
     
     let moveQuestion = document.createElement("div");
     moveQuestion.classList.add("moveQuestion");
 
     let scoreDiv = document.createElement("div");
     scoreDiv.classList.add("scoreDiv")
-    
-    const questionButton2 = document.createElement("button");
-    questionButton2.innerText = question.incorrect_answers[0];
-    const questionButton3 = document.createElement("button");
-    questionButton3.innerText = question.incorrect_answers[1];
-    const questionButton4 = document.createElement("button");
-    questionButton4.innerText = question.incorrect_answers[2];        
 
     const previousButton = document.createElement("button");
     previousButton.innerText = "Anterior";
@@ -121,7 +128,6 @@ function renderTrivia(dataArray, index){
     
     if(question.type == "multiple"){
         buttonContainer.appendChild(questionButton1);
-        questionButton1.classList.add("correct");
         questionButton1.classList.add("btnTrivia");
         buttonContainer.appendChild(questionButton2);
         questionButton2.classList.add("btnTrivia");
@@ -132,7 +138,6 @@ function renderTrivia(dataArray, index){
 
     }else{
         buttonContainer.appendChild(questionButton1);
-        questionButton1.classList.add("correct");
         questionButton1.classList.add("btnTrivia");
         buttonContainer.appendChild(questionButton2);
         questionButton2.classList.add("btnTrivia");
@@ -141,56 +146,64 @@ function renderTrivia(dataArray, index){
     moveQuestion.appendChild(previousButton);
     moveQuestion.appendChild(nextButton);
 
-    scoreDiv.appendChild(scoreHeader);
-    
+    scoreDiv.appendChild(scoreHeader);    
 }
 
 function addClickTrivia (){
     let getButton = document.querySelectorAll(".btnTrivia");
     let header = document.querySelectorAll(".scoreHeader");
+    
     for(let button of getButton){
-        button.addEventListener("click", function(event){
-            if(button.className.match("correct btnTrivia")){
-                score += 1;
-                button.classList.add("w");
-                header[0].innerText = `Tu Puntuacion es: ${score}`;
-                getButton[0].disabled = true;
-                getButton[1].disabled = true;
-                getButton[2].disabled = true;
-                getButton[3].disabled = true;
-            }else{
-                button.classList.add("incorrect");
-                getButton[0].disabled = true;
-                getButton[1].disabled = true;
-                getButton[2].disabled = true;
-                getButton[3].disabled = true;
-            }
+        for(let i = 0;i<getButton.length;i++){
+            getButton[i].classList.add(i);
+        }   
+        button.addEventListener("click", function(event){            
+                if(button.className.match("0")){
+                    score += 1;
+                    button.classList.add("w");
+                    header[0].innerText = `Tu Puntuacion es: ${score}`; 
+                    answers[indice] = 0;               
+                    disableButtons(getButton);
+                }else{
+                    for(let j = 1; j < getButton.length; j++){
+                        if(button.className.match(j)){
+                            answers[indice] = j;
+                        }
+                    }
+                    button.classList.add("incorrect");
+                    disableButtons(getButton);
+                    
+                }        
+                finishTrivia(dataArray);     
+                console.log(answers);                   
         })
-    }
-
+    }    
 }
 
 function previousQuestion(){
     let previousButton = document.querySelectorAll(".btnMovePrevious");
-    console.log(previousButton[0])
+
     previousButton[0].addEventListener("click", function (event){
         if(indice > 0){
             indice = indice - 1;
             removeContainers("questionDiv");
-            renderList();
+            renderList(indice);
+            let buttons = document.querySelectorAll(".btnTrivia");
+            checkResponse(buttons);
         }
     })
 }
 
 function nextQuestion(){
-    let nextButton = document.querySelectorAll(".btnMoveNext");
+    let nextButton = document.querySelectorAll(".btnMoveNext");   
+
     nextButton[0].addEventListener("click", function (event){        
         if(indice < lengthArray-1){
-            console.log(indice)
-            indice = indice + 1;            
-            
+            indice = indice + 1; 
             removeContainers("questionDiv");
-            renderList();
+            renderList(indice);
+            let buttons = document.querySelectorAll(".btnTrivia");
+            checkResponse(buttons);
         }
     })
 }
@@ -202,6 +215,76 @@ function removeContainers (classItem){
     }
 }
 
-function checkResponse(){
-    console.log("gatis")
+function disableButtons(buttons){
+    for(let i = 0;i<buttons.length;i++){
+        buttons[i].disabled = true;
+    }   
+}
+
+function checkResponse(buttons){    
+    if(answers[indice] != null){
+        for(button of buttons){
+            if(button.className.match(answers[indice])){
+                if(answers[indice] == 0){
+                    button.classList.add("w");
+                }else{
+                    button.classList.add("incorrect");
+                }                        
+            }
+        }
+        disableButtons(buttons);                
+    }
+}
+
+function checkArray (array){
+    if(array.response_code!= 0){
+        alert("No se puede generar la trivia sin preguntas");
+        console.log("No se puede generar la trivia sin preguntas");
+    }else{
+        renderList(indice);
+    }
+}
+
+function makeHeader(){
+    const header = document.createElement("h2");
+    header.innerText = `Resumen: Tu Puntuacion fue ${score}`;
+    header.classList.add("headerSummary")
+    let triviaDiv = document.getElementById("trivia");
+    triviaDiv.appendChild(header);
+}
+
+function btnRefresh(){
+    let refreshButton = document.createElement("button");
+    refreshButton.innerText = "Jugar Otra Vez";
+    refreshButton.classList.add("btnRefresh");
+    let triviaDiv = document.getElementById("trivia");
+    triviaDiv.appendChild(refreshButton);
+    refreshButton.addEventListener("click", function(event){
+        location.reload();
+    })
+}
+
+function finishTrivia(datas){
+    if(!answers.includes(null)){
+        removeContainers("questionDiv");
+        makeHeader();
+        btnRefresh();
+        for(let i = 0; i <datas.results.length; i++){
+            renderTrivia(datas, i, i); 
+            removeContainers("headerTrivia");
+            removeContainers("moveQuestion");
+            removeContainers("scoreDiv");
+            let children = document.getElementById(i).children;            
+            for(let j = 0;j < children.length; j++){
+                if(answers[i] == j){
+                    if(answers[i] == 0){
+                        children[j].classList.add("w");
+                    }else{
+                        children[j].classList.add("incorrect");
+                    }
+                }
+            }
+        }                 
+        console.log("Finished Game");
+    }
 }
